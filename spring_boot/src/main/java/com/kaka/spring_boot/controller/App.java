2f4a49af742c22b7f99b671a5e8596b1715fc29b
itcast <bos@itcast.cn>
2017-12-30 08:32:46 +0800

    qq

---
+++ spring_boot/src/main/java/com/kaka/spring_boot/controller/App.java
@@ -0,0 +1,14 @@
+package com.kaka.spring_boot.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(App.class, args);
+		
+	}
+}
---
+++ spring_boot/src/main/java/com/kaka/spring_boot/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.kaka.spring_boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@RequestMapping("hello")
+	public String hello(){
+		return "hello";
+	}
+	
+}
